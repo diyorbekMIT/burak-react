1f875d2faf422a6af1f1fdb5f9d62a9bd474ee51
diyorbekMIT <diyorbekjon2202@gmail.com>
2024-07-05 12:45:13 +0900

    feat:React install and config

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
 import '../css/App.css';
+import { Button, Container, Typography, Stack, Box } from '@mui/material';
+import { RippleBadge } from './MaterialTheme/styled';
 
 function App() {
-  return <div>HOME</div>;  
+  return (
+    <Container maxWidth="sm">
+      <Stack direction="column" spacing={2} alignItems="center">
+        <Box sx={{ my: 4 }}>
+          <Typography variant="h4" component="h4">
+            Create React App with TypeScript and Redux
+          </Typography>
+        </Box>
+        <Box>
+          <RippleBadge badgeContent={4}>
+            <Button variant="contained" color="primary">
+              Contained
+            </Button>
+          </RippleBadge>
+        </Box>
+      </Stack>
+    </Container>
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,12 @@ import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './app/App';
 import reportWebVitals from './reportWebVitals';
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from '@mui/material/styles';
+
 import './css/index.css';
+import theme from './app/MaterialTheme';
+
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
@@ -12,7 +17,9 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>
 );
