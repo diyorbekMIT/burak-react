5c4bb52074b0a329ef26b797a1c5c7991d8479e0
diyorbekMIT <diyorbekjon2202@gmail.com>
2024-07-13 22:56:38 +0900

    feat: ProductsPage (64-65)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,36 +1,44 @@
-import React from 'react';
-import {Link, Route, Switch, useLocation } from 'react-router-dom';
-// import { RippleBadge } from './MaterialTheme/styled';
-import { Box, Button, Container, Stack, Typography } from '@mui/material';
-import {  HomePage } from './screens/homePage';
-import { ProductsPage } from './screens/productsPage';
-import { OrdersPage } from './screens/ordersPage';
-import { UserPage } from './screens/userPage';
-import { HomeNavbar } from './components/headers/HomeNavbar';
-import { OtherNavbar } from './components/headers/OtherNavbar';
-import  Footer  from './components/footer';
-import '../css/App.css';
+import { Route, Switch, useLocation } from "react-router-dom";
+import { OrdersPage } from "./screens/ordersPage";
+import { UserPage } from "./screens/userPage";
+import { HomeNavbar } from "./components/headers/HomeNavbar";
+import { OtherNavbar } from "./components/headers/OtherNavbar";
+import "../css/App.css";
+import "../css/navbar.css";
 import "../css/navbar.css";
 import "../css/footer.css";
-import { HelpPage } from './screens/helpPage';
-function App() {
-  const location  = useLocation()
-  console.log("console:" , location);
+import { HelpPage } from "./screens/helpPage";
+import Footer from "./components/footer";
+import { HomePage } from "./screens/homePage";
+import { ProductsPage } from "./screens/productsPage";
+
+export function App() {
+  const location = useLocation();
+  console.log(location);
+
   return (
     <>
-      {location.pathname === '/' ? <HomeNavbar/> : <OtherNavbar/> }
+      {location.pathname === "/" ? <HomeNavbar /> : <OtherNavbar />}
       <Switch>
-        <Route path="/"><HomePage/> </Route>
-        <Route path="/products"><ProductsPage /></Route>
-        <Route path="/orders"><OrdersPage /></Route>
-        <Route path="/member-page"><UserPage/></Route>
-        <Route path="/help"><HelpPage /></Route>
+        <Route path="/products">
+          <ProductsPage />
+        </Route>
+        <Route path="/orders">
+          <OrdersPage />
+        </Route>
+        <Route path="/member-page">
+          <UserPage />
+        </Route>
+        <Route path="/help">
+          <HelpPage />
+        </Route>
+        <Route path="/">
+          <HomePage />
+        </Route>
       </Switch>
-      <Footer/>
+      <Footer />
     </>
   );
 }
-
-
 
 export default App;
--- src/app/screens/productsPage/index.tsx
+++ src/app/screens/productsPage/index.tsx
@@ -1,6 +1,20 @@
-import { Container } from "@mui/material";
+import React from 'react';
+import { Route, Switch, useRouteMatch} from "react-router-dom";
+ import { Container } from "@mui/material";
+import ChoosenProduct from './ChosenProduct';
+import Products from './Products';
 
 export function ProductsPage() {
-    return <Container>ProductsPage</Container>;
+    const products = useRouteMatch();
+    return <div className='products-page'>
+      <Switch>
+        <Route path={`${products.path}/:productId`}>
+          <ChoosenProduct />
+        </Route>
+        <Route path={`${products.path}`}>
+          <Products />
+        </Route>
+      </Switch>
+    </div>
   }
   
