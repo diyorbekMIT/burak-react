2e636008669df14c7c2d62b69eba4fb6b4bdc595
diyorbekMIT <diyorbekjon2202@gmail.com>
2024-08-04 16:07:23 +0900

    feat: slice and selectors

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import PopularDishes from "./PopularDishes";
 import Statistics from "./Statistics";
 import NewDishes from "./NewDishes";
@@ -7,7 +7,33 @@ import Advertisement from "./Advertisement";
 import ActiveUsers from "./ActiveUser";
 import "../../../css/home.css";
 
+import { useDispatch, useSelector } from "react-redux";
+import { Dispatch } from "@reduxjs/toolkit"; 
+import { createSelector } from "reselect";
+import { setPopularDishes } from "./slice";
+import { retrievePopularDishes } from "./selector";
+import { Product } from "../../../lib/types/product";
+
+
+/** REDUX SLIX & SELECTOR */ 
+
+const acttionDispatch = (dispatch: Dispatch) => ({
+    setPopularDishes: (data: Product[]) => dispatch(setPopularDishes(data))
+});
+const popularDishesRetriever = createSelector(
+    retrievePopularDishes,
+    (popularDishes) => ({popularDishes})
+)
 export function HomePage() {
+    // Selector: Store => Data
+
+    const {setPopularDishes} = acttionDispatch(useDispatch());
+    const {popularDishes} = useSelector(popularDishesRetriever);
+
+    useEffect (() => {
+        //Backend server data request => Data
+        // Slice: Data => Store
+    }, []);
     return (
         <div className="homepage">
             
