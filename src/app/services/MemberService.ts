a717640dff24b4464cc8e638387c5b09fc257f9c
diyorbekMIT <diyorbekjon2202@gmail.com>
2024-08-07 09:49:35 +0900

    feat: create login process

--- src/app/services/MemberService.ts
+++ src/app/services/MemberService.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { serverApi } from "../../lib/config";
-import { Member, MemberInput } from "../../lib/types/members";
+import { LoginInput, Member, MemberInput } from "../../lib/types/members";
 import { Product } from "../../lib/types/product";
 
 class MemberService { 
@@ -53,8 +53,24 @@ class MemberService {
         }
     }
 
-    
+    public async login(input: LoginInput): Promise<Member> {
+        try {
+            const url = this.path + "/member/login";
+            const result = await axios.post(url, input, {withCredentials: true});
+            console.log("login: ", result);
 
+            const member: Member = result.data.member;
+            console.log("member: ", member);
+            localStorage.setItem("memberData", JSON.stringify(member));
+
+            return member;
+        } catch (err) {
+            console.log("Error, login:", err);
+            throw err;
+        }
+    }
+
+    
 }
 
 export default MemberService;
